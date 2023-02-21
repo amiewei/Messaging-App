d2a3b10e0d6c7378f6564ee2a18f3e0ac71b4e54
Amie Wei <amie.wei12@gmail.com>
2023-02-20 18:44:27 -0600

    added firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,20 +2,20 @@ import { getAnalytics } from "firebase/analytics";
 import "firebase/compat/auth";
 import "firebase/auth";
 import firebase from "firebase/compat/app";
-import firebaseConfig from "../../firebase.config.js";
+// import firebaseConfig from "../../firebase.config.js";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAIcwnE9K3NuoI_R0bkrX2ynd5xAS4xru8",
-//   authDomain: "express-mdn.firebaseapp.com",
-//   projectId: "express-mdn",
-//   storageBucket: "express-mdn.appspot.com",
-//   messagingSenderId: "30505840910",
-//   appId: "1:30505840910:web:50967432d19bd90b7ea75f",
-//   measurementId: "G-3305S8SJRK",
-// };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "express-mdn.firebaseapp.com",
+  projectId: "express-mdn",
+  storageBucket: "express-mdn.appspot.com",
+  messagingSenderId: "30505840910",
+  appId: "1:30505840910:web:50967432d19bd90b7ea75f",
+  measurementId: "G-3305S8SJRK",
+};
 
 // Initialize Firebase with the firebase SDK
 
