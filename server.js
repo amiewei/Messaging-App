c9af3d57a8ce70bfccded23485fdbea320d72b92
Amie Wei <amie.wei12@gmail.com>
2023-02-25 15:18:00 -0600

    redirect middleware

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ var path = __dirname + "/dist";
 var port = 8080;
 
 app.use(express.static(path));
+
+app.use((req, res, next) => {
+  var host = req.get("Host");
+  console.log("redirect middleware");
+  console.log(host);
+  console.log(req.originalUrl);
+  if (host.includes("auth-client-ol1w.onrender.com")) {
+    console.log("redirecting");
+    return res.redirect(
+      301,
+      "https://https://chirp.ddns.net/" + req.originalUrl
+    );
+  }
+  return next();
+});
+
 app.get("*", function (req, res) {
   console.log("catch all");
   console.log(path);
